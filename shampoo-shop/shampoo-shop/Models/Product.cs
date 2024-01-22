739ec7d6bf69ff28ef534dbb0e6f9d8fc610e12c
Khang <xkhang.nguyen2bme@gmail.com>
2024-01-22 10:38:55 +0700

    create shampoo model

---
+++ shampoo-shop/shampoo-shop/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shampoo_shop.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        public string Ingredients { get; set; }
+
+        [Required]
+        public string Effect { get; set; }
+
+        [Required]
+        public string Usage { get; set; }
+	}
+}
+
